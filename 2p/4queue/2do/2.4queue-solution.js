32ccce0ca934db80c66142db3898c7a5c5e4e7d9
MARIN OLIVO OLIVO <113148840+AzerinaMarin@users.noreply.github.com>
2022-10-06 23:48:53 +0000

    primera actualización

--- 2p/4queue/2do/2.4queue-solution.js
+++ 2p/4queue/2do/2.4queue-solution.js
@@ -1,5 +1,31 @@
-/** 
- *
- * your solution here
- *
- */
+function Queue(){
+this.dataStore=[]
+  this.top =5
+
+  
+  this.enqueue= function(element) {
+	this.dataStore.push(element)
+    }
+    this.dequeue= function() {
+	return this.dataStore.pop()
+    }  
+  this.empty= function(){
+    	if (this.dataStore.length == 0)
+	    return true	
+	else 
+	    return false
+  }
+  this.full= function() {
+       if (this.data.length === this.top)
+            return true
+        else
+            return false
+  }
+}
+
+let q = new Queue()
+q.enqueue("Mere")
+q.enqueue("Oded")
+q.enqueue("Ana")
+q.enqueue("Bana")
+q.enqueue("Hanna")
